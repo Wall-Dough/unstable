8c5538a25135f3a7539bae0e0eeddb2d1fbab0f6
Wall-Dough <awalden0808@gmail.com>
2021-10-02 13:59:58 -0500

    Add monster enraged state

---
+++ monster/Monster.gd
@@ -0,0 +1,35 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+var active_sprite
+var rage_level = 0
+var max_rage = 100
+var enraged = false
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	active_sprite = $body/collision/sprite/rest
+
+func change_sprite(sprite):
+	active_sprite.hide()
+	active_sprite = sprite
+	active_sprite.show()
+
+func enrage():
+	enraged = true
+	change_sprite($body/collision/sprite/enraged)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if !enraged:
+		rage_level += delta * 10
+		if rage_level >= max_rage:
+			rage_level = max_rage
+			enrage()
+	var max_rage_size = $body/max_rage.get_size().x
+	var rage_size = rage_level / max_rage * max_rage_size
+	$body/max_rage/rage_level.rect_size.x = rage_size
