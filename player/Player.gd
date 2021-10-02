65948f055c6174d8e4bb7662212b6b08e0217cb1
Wall-Dough <awalden0808@gmail.com>
2021-10-02 16:16:56 -0500

    Keep player still while performing action

--- player/Player.gd
+++ player/Player.gd
@@ -105,10 +105,11 @@ func _process(delta):
 		if cooldown_left <= 0:
 			cooldown_left = 0
 	var direction = 0
-	if Input.is_action_pressed("move_left"):
-		direction -= 1
-	if Input.is_action_pressed("move_right"):
-		direction += 1
+	if cooldown_left == 0:
+		if Input.is_action_pressed("move_left"):
+			direction -= 1
+		if Input.is_action_pressed("move_right"):
+			direction += 1
 	$body.set_direction(direction)
 	if Input.is_action_just_pressed("jump"):
 		$body.jump()
